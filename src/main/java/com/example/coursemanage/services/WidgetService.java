5663a6f8cad80c0f0bfe12df395571eebfaf1929
Shiyu Wang <shiyu3169@gmail.com>
2018-09-07 10:32:53 -0400

    loading widgets

--- src/main/java/com/example/coursemanage/services/WidgetService.java
+++ src/main/java/com/example/coursemanage/services/WidgetService.java
@@ -8,6 +8,7 @@ import com.example.coursemanage.repositories.WidgetRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,11 @@ public class WidgetService {
     
     @Autowired
     WidgetRepository widgetRepository;
+
+    @GetMapping("/api/widget")
+    public List<Widget> findAllWidgets() {
+        return (List<Widget>) widgetRepository.findAll();
+    }
 
     @PostMapping("/api/widget")
     public List<Widget> saveWidgets(@RequestBody List<Widget> widgets) {
